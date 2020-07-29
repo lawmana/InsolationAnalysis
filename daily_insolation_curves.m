ec941291932a18bd1e113cf2d0af2599b6a49d23
Allison Lawman <lawmana@gmail.com>
2020-07-29 10:20:20 -0500

    Updated plot formatting and comments

--- daily_insolation_curves.m
+++ daily_insolation_curves.m
@@ -1,20 +1,27 @@
 %==========================================================================
-% DAILY INSOLATION CURVES AS A FUNCTION OF LATITUDE AND DAY OF YEAR
+% DAILY TOP-OF-ATMOSPHERE (TOA) INSOLATION CURVES AS A FUNCTION OF 
+% LATITUDE AND DAY OF YEAR
 
 % Calculate daily insolation for past time intervals at every latitude
-% Compare the total incoming solar radiation to present day (0 ka)
-
-% Use daily_insolation.m - Ian Eisenman and Peter Huybers, August 2006
+% Compare the insolation to present day (0 ka)
+%--------------------------------------------------------------------------
+% REQUIRED FUNCTIONS & TOOLBOXES:
+% daily_insolation.m - Ian Eisenman and Peter Huybers, August 2006
 % Modified for the Climate Data Toolbox: http://www.chadagreene.com/CDT/daily_insolation_documentation.html
 
 % Use the default option 1 for 'day_type' that specifies days in the range 
 % 1 to 365.24, where day 1 is January first and the vernal equinox always 
 % occurs on day 80
 
+% cmocean: colormaps for oceanography available for Matlab:
+% https://www.mathworks.com/matlabcentral/fileexchange/57773-cmocean-perceptually-uniform-colormaps
+%--------------------------------------------------------------------------
 % References:
 % Berger A. and Loutre M.F. (1991). Insolation values for the climate of the last 10 million years. Quaternary Science Reviews, 10(4), 297-317.
 % Berger A. (1978). Long-term variations of daily insolation and Quaternary climatic changes. Journal of Atmospheric Science, 35(12), 2362-2367.
-% Huybers, Peter. "Early Pleistocene glacial cycles and the integrated summer insolation forcing." science 313.5786 (2006): 508-511.
+% Huybers, Peter (2006). Early Pleistocene glacial cycles and the integrated summer insolation forcing. Science, 313.5786, 508-511.
+% Emile-Geay et al. (2016). Links between tropical Pacific seasonal, interannual and orbital variability during the Holocene. Nature Geoscience, 9(2), 168-173. 
+% Thyng, K.M., C.A. Greene, R.D. Hetland, H.M. Zimmerle, and S.F. DiMarco. 2016. True colors of oceanography: Guidelines for effective and accurate colormap selection. Oceanography 29(3):9?13.
 %==========================================================================
 close all; clc; 
 
@@ -22,11 +29,24 @@ close all; clc;
 saveLoc = [mfilename('fullpath') '/'];
 mkdir(saveLoc);
 
-% selected past time intervals (thousands of years)
-kyears = 0:3:30;
+%=====================================================
+% USER INPUTS: SELECTED LATITUDES AND TIME INTERVALS
+%=====================================================
+
+% selected past time intervals (start, end and increment) in units
+% thousands of years before present (ka)
+start_ka = 0;
+end_ka = 30;
+incYr = 3;
 
 % selected latitudes
 lats = -90:90;
+
+%=====================================================
+% LATITUDE vs. DAILY INSOLATION
+%=====================================================
+% define vector of selected kyears
+kyears = start_ka:incYr:end_ka;
 
 % use meshgrid to create grids of the day and latitude variables
 [day,lat]=meshgrid(1:365, lats);
@@ -34,28 +54,30 @@ lats = -90:90;
 % preallocate matrix to store insolation values for each day and latitude
 Fsw = zeros([size(lat), length(kyears)]);
 
-% iterate over the kyrs, storing the daily insolation at each latitude
+% iterate over the kyears, storing the daily insolation at each latitude
 for i = 1:length(kyears)
 
     Fsw(:,:,i) =daily_insolation(kyears(i),lat,day);  
     
 end
 
-%======================================
-% FIGURE GENERATION
-%======================================
+%-------------------------------------------------------
+%% FIGURE GENERATION
+%-------------------------------------------------------
+
 monthLabels = {'Jan','Feb','Mar','Apr','May','Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'};
 
-%------------------------------------------------
-% Selected orbital configuration (e.g., 0 ka)
-%------------------------------------------------
+%-------------------------------------------------------
+% Orbital configuration at slelected time interval
+% (e.g., 0 ka)
+%-------------------------------------------------------
 selectedKyr = 0; % ka
 
 % index for selected time interval
 iKyr = find(kyears == selectedKyr);
 
 figure(1); clf; hold on;
-title(['Insolation (' num2str(kyears(iKyr)) ' ka)'])
+title(['TOA Insolation (' num2str(kyears(iKyr)) ' ka)'])
 % colormap with formatted color bar
 pcolor(day, lat, Fsw(:,:,iKyr))
 shading interp
@@ -87,9 +109,9 @@ ax.FontWeight = 'bold';
 figName = ['DailyInsolation_' num2str(kyears(iKyr)) '_ka'];
 print([saveLoc figName '.png'], '-dpng');
 
-%------------------------------------------------
-%% Difference in insolation relative to present
-%------------------------------------------------
+%=====================================================
+%% Difference in insolation relative to present (0 ka)
+%=====================================================
 for i = 2:length(kyears)
     
     % calculate difference relative to 0 ka
@@ -97,7 +119,7 @@ for i = 2:length(kyears)
     
     % plot
     figure(i); clf; hold on;
-    title(['Insolation (' num2str(kyears(i)) ' - 0 ka)'])
+    title(['TOA Insolation (' num2str(kyears(i)) ' - 0 ka)'])
     
     % color map
     pcolor(day, lat, Fsw_diff)
